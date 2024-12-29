29ee556081159b3cf5a8c84ed3b90f04c3fe8497
DevMaan707 <dev.maan707@gmail.com>
2024-12-29 11:34:05 +0530

    changed class name

--- lib/cc_essentials.dart
+++ lib/cc_essentials.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import 'services/shared_preferences/shared_preference_service.dart';
 import 'theme/custom_theme.dart';
 
-class CC_Essentials {
+class CCEssentials {
   static Future<void> initialize({
     required Color primaryColor,
     required Color accentColor,
